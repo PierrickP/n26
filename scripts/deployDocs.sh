26990b83ceca85d88dca6c1de25ed7cc53170976
Pierrick <mulotmail@gmail.com>
2016-05-29 16:22:58 +0200

    Fix travis deploy

--- scripts/deployDocs.sh
+++ scripts/deployDocs.sh
@@ -2,11 +2,11 @@
 
 GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository.url)')}
 
-cd dist && \
+cd docs && \
 rm -rf .git
 git init && \
 git config user.name \"Travis CI\" && \
 git config user.email \"github@travis-ci.org\" && \
 git add . && \
-git commit -m "Deploy to GitHub Pages" && \
+git commit -m "Deploy docs for version $(node -e 'process.stdout.write(require("../package.json").version)')" && \
 git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
